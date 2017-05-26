4138d424c0615f47baa1d33698a2015092861221
Robert Parkinson <robertparkinson@gmail.com>
2017-05-26 13:14:12 +0100

    copied files form t1 prototype

--- app/routes.js
+++ app/routes.js
@@ -9,11 +9,19 @@ router.get('/', function (req, res) {
 // add your routes here
 
 router.post('*/choose-how-to-pay', function (req, res) {
-  if (req.body.paymentType === 'card') {
+  if (req.body.paymentType == 'card') {
     res.redirect('pay-by-card')
   } else {
     res.redirect('pay-by-account')
   }
+})
+
+router.post('*/claim-interest', function (req, res) {
+    if (req.body.interestRate == 'No interest') {
+        res.redirect('claim-total')
+    } else {
+        res.redirect('claim-interest-date')
+    }
 })
 
 module.exports = router
