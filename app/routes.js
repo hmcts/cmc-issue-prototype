cb2f61a6f79d1169039b64ad648b2e41465d1473
Robert Parkinson <robertparkinson@gmail.com>
2017-05-31 15:33:47 +0100

    removed tasklist

--- app/routes.js
+++ app/routes.js
@@ -8,13 +8,6 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
-router.post('*/choose-how-to-pay', function (req, res) {
-  if (req.body.paymentType == 'card') {
-    res.redirect('pay-by-card')
-  } else {
-    res.redirect('pay-by-account')
-  }
-})
 
 router.post('*/claim-interest', function (req, res) {
     if (req.body.interestRate == 'No interest') {
--- app/views/prototype-may-2017/routes/routes.js
+++ app/views/prototype-may-2017/routes/routes.js
@@ -11,21 +11,38 @@ module.exports = function(app){
 
     app.post('*/prototype-may-2017/what-type-of-claimant', function(req, res){
         if (req.body.claimantType.toString() === 'company') {
-            res.redirect('company-details')
+            res.redirect('claimant-company-reference')
         }
         else {
-            res.redirect('claimant-name')
+            res.redirect('claimant-individual-reference')
         }
     });
 
     app.post('*/prototype-may-2017/defendant-type', function(req, res){
 
-        console.log(req.body.defendantType)
         if (req.body.defendantType.toString() === 'company') {
             res.redirect('defendant-company-details')
         }
         else {
             res.redirect('defendant-details')
+        }
+    });
+
+    app.post('*/prototype-may-2017/type-of-claim', function(req, res){
+
+        if (req.body.typeOfClaim.toString() === 'specified') {
+            res.redirect('not-supported')
+        }
+        else {
+            res.redirect('claim-amount')
+        }
+    });
+
+    app.post('*/choose-how-to-pay', function (req, res) {
+        if (req.body.paymentType == 'card') {
+            res.redirect('pay-by-card')
+        } else {
+            res.redirect('pay-by-account')
         }
     });
 
